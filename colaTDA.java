796bf01329db16c066498ce157abcaa7e268029b
OctavioCosentino <163548530+OctavioCosentino@users.noreply.github.com>
2024-03-19 12:56:06 +0000

    ColaTDA

--- colaTDA.java
+++ colaTDA.java
@@ -11,3 +11,45 @@ public void PasarCola(ColaTDA Origen, ColaTDA Destino){
         Origen.Desacolar();
     }
 }
+
+public int AuxiliarFinales(ColaTDA Origen){
+    PilaTDA aux = new PilaTDA()
+    while(!Origen.ColaVacia()){
+        aux.Apilar(Origen.Primero());
+        Origen.Desacolar();
+    }
+    return aux.Tope();
+}
+
+public boolean FialesIguales(ColaTDA C1, ColaTDA C2){
+    boolean flag = false;
+    int FinalC1 = AuxiliarFinales(C1);
+    int FinalC2 = AuxiliarFinales(C2);
+    if (FinalC1 == FinalC2){
+        flag = true
+    }
+    return flag
+}
+
+public boolean ColasInversas(ColaTDA C1, ColaTDA C2){
+    PilaTDA aux = new PilaTDA()
+    boolean flag = false;
+    int cont;
+    int cont2;
+    while(!Origen.ColaVacia()){
+        aux.Apilar(Origen.Primero());
+        cont2++;
+        Origen.Desacolar();
+    }
+    while(!aux.PilaVacia()){
+        if(aux.Tope() == C2.Primero()){
+            cont++;
+        }
+        aux.Desapilar();
+        C2.Desacolar();
+        if (cont == cont2){
+            flag = true;
+        }
+        return flag
+    }
+}
